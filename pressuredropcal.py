8dc2f1b48334ee9135d8ad0224c73e16f08085f4
Joey <maoweixin1995@gmail.com>
2024-12-18 20:42:20 +0800

    20241218 commit

--- pressuredropcal.py
+++ pressuredropcal.py
@@ -18,11 +18,31 @@ class PressureDropCalculator(QtWidgets.QMainWindow):
         #创建顶部分割器
         top_splitter = QtWidgets.QSplitter(QtCore.Qt.Orientation.Horizontal)
         
+        #左侧：组件列表
+        left_panel = self.create_left_panel()
+        top_splitter.addWidget(left_panel)
         
+        #右侧：画布区域
+        self.scene = QtWidgets.QGraphicsScene()
+        self.right_panel = QtWidgets.QGraphicsView(self.scene)
+        self.right_panel.setSizePolicy(QtWidgets.QSizePolicy.Policy.Expanding,QtWidgets.QSizePolicy.Policy.Expanding)
+        top_splitter.addWidget(self.right_panel)
+        
+        #设置分割器比例
+        top_splitter.setSizes([150,650])
         
         main_layout.addWidget(top_splitter)
         
         self.show()
+        
+    def create_left_panel(self):
+        #左侧面板用于显示组件列表
+        left_pannel = QtWidgets.QListWidget()
+        left_pannel.addItem("Pipe")
+        left_pannel.addItem("Valve")
+        left_pannel.addItem("Elbow")
+        left_pannel.setMinimumWidth(150)
+        return left_pannel
         
 if __name__== "__main__":
     import sys
